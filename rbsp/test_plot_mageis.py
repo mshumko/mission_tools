ff86cf18247b0ec69406f30b95da7d98bc82c50d
mshumko <msshumko@gmail.com>
2017-11-11 20:34:48 -0700

    Updated the unittest file to test rel03 and highrate plotting for both RBSP spaecracft.

--- rbsp/test_plot_mageis.py
+++ rbsp/test_plot_mageis.py
@@ -13,27 +13,28 @@ T_BOUNDS = [datetime(2017, 3, 31, 11, 15), datetime(2017, 3, 31, 11, 20)]
 class TestMagEISLib(unittest.TestCase):
 
     def test_highrate(self):
-        fig, ax = plt.subplots()
-        fluxObj = plot_mageis.magEISspectra(RB_ID, DATE, dataLevel=3)
-        fluxObj.tBounds = T_BOUNDS
-        fluxObj.loadMagEIS(instrument='LOW', highrate=True)
-        fluxObj.plotHighRateTimeSeries(ax=ax, smooth=10)
+        for rb_id in ['A', 'b']:
+            fig, ax = plt.subplots()
+            hrObj = plot_mageis.PlotMageis(rb_id, DATE, 'highrate', tRange=T_BOUNDS, 
+                instrument='low')
+            hrObj.plotAlpha(ax=ax)
+            hrObj.plotTimeseries(ax=ax)
 
-    # def test_int(self):
-    #     fig, ax = plt.subplots()
-    #     fluxObj = plot_mageis.magEISspectra(RB_ID, DATE, dataLevel = 3)
-    #     fluxObj.tBounds = T_BOUNDS
-    #     fluxObj.loadMagEIS(highrate=False, relType='int')
-    #     fluxObj.plotHighRateTimeSeries(ax=ax, smooth = 10, n_sectors=11)
-        
+    #     rb_id = 'A'
+    # date = datetime(2017, 3, 31)
+    # tRange = [datetime(2017, 3, 31, 11, 15), datetime(2017, 3, 31, 11, 25)]
     
     def test_rel03(self):
-        fig, ax = plt.subplots()
-        fluxObj = plot_mageis.magEISspectra(RB_ID, DATE, dataLevel = 3)
-        fluxObj.tBounds = T_BOUNDS
-        fluxObj.loadMagEIS(highrate=False, relType='rel03')
-        fluxObj.plotHighRateTimeSeries(ax=ax, smooth = 10, n_sectors=11)
-        #fluxObj.plotHighRateSpectra(E_ch = 1, scatterS = 50)
+        for rb_id in ['A', 'b']:
+            fig, ax = plt.subplots()
+            rel03Obj = plot_mageis.PlotMageis(rb_id, DATE, 'rel03', tRange=T_BOUNDS)
+            rel03Obj.plotUnidirectionalFlux(90, ax=ax)
+            rel03Obj.plotEpinAvgSpectra(ax=ax)
+        # fluxObj = plot_mageis.magEISspectra(RB_ID, DATE, dataLevel = 3)
+        # fluxObj.tBounds = T_BOUNDS
+        # fluxObj.loadMagEIS(highrate=False, relType='rel03')
+        # fluxObj.plotHighRateTimeSeries(ax=ax, smooth = 10, n_sectors=11)
+        # #fluxObj.plotHighRateSpectra(E_ch = 1, scatterS = 50)
         
         
 
