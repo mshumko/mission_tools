3ac44760f97372cd10e0d9f0af264f1ac4c17936
mshumko <msshumko@gmail.com>
2018-03-04 10:45:55 -0700

    Updated the download scripts for MagEIS, EMFISIS, and the MagEphem files for RBSP. The user can specifcy command line arguments to download data from a particular day, or range of days. Can also specify the spacecarft to download data from. Call the scrips with: python3 SCRIPT.py -h to get more info.

--- rbsp/download_day_emfisis.py
+++ rbsp/download_day_emfisis.py
@@ -22,14 +22,14 @@ def findEMFISISSpectraUrl(sc_id, date, **kwargs):
     splitDate = date.date().isoformat().split('-')
     
     if url is None:
-        url = ('http://emfisis.physics.uiowa.edu/Flight/RBSP-' + sc_id + '/L2/' + 
+        url = ('http://emfisis.physics.uiowa.edu/Flight/RBSP-' + sc_id.upper() + '/L2/' + 
         splitDate[0] + '/' + splitDate[1] + '/' +  splitDate[2]) 
           
     response = urllib.request.urlopen(url)
     data = response.read().decode()
-    print(hour)
-    print('{}-{}_emfisis-L2_{}{}'.format(detectorType, spectraType, 
-        ''.join(splitDate), hour))
+    # print(hour)
+    # print('{}-{}_emfisis-L2_{}{}'.format(detectorType, spectraType, 
+    #     ''.join(splitDate), hour))
     ind = data.find('{}-{}_emfisis-L2_{}{}'.format(detectorType, spectraType, 
         ''.join(splitDate), hour))
     lastInd = data.find('.cdf', ind)
@@ -59,23 +59,70 @@ def saveEMFISISSpectra(sc_id, date, fPath, fName = None, **kwargs):
     return
 
 if __name__ == '__main__':
-    # date = datetime(2013,8,27)
-    # sc_id = 'A'
-    # dataUrl = findEMFISISSpectraUrl(sc_id, date)
-    # saveEMFISISSpectra(sc_id, date, '/home/mike/research/rbsp/data/emfisis/'
-    #     'rbsp{}'.format(sc_id.lower()) )
-    
-    startDate = datetime(2015, 2, 2)
-    endDate =  datetime(2015, 2, 2)
+    # This section of the code sets up the argument parser.
+    import argparse
+    import itertools
+    parser = argparse.ArgumentParser(description=('This script downloads the '
+            'EMFISIS data from a website defined in the code.'))
+    parser.add_argument('-sc', '--sc_id', help=('Define a spaceraft id. '
+                                            'Use either "a" or "b". If '
+                                            'none, will download data '
+                                            'from both RBSP spacecraft.'),
+                                            type=str, nargs=1)
+    parser.add_argument('date', default=[], nargs='*', type=int,
+                        help=('This is an optional date positional argument.'
+                                ' The format is YYYY MM DD.'))
+    parser.add_argument('-dr', '--dateRange', default=[], nargs='*', type=int,
+                            help=('This optional argument passes in a start'
+                                    ' and end dates to define a date range for '
+                                    'which the script will download EMFISIS data.'
+                                    ' The format is YYYY MM DD and the input is '
+                                    'size 6'))
+    args = parser.parse_args()
+    if args.date != []:
+        if len(args.date) != 3:
+            raise ValueError('Date not formated correctly! Check manual.')
+        startDate = datetime(*args.date)
+        endDate = datetime(*args.date)
+    if args.dateRange != []:
+        if len(args.dateRange) != 6:
+            raise ValueError('Date range not formated correctly! Check manual.')
+        startDate = datetime(*args.dateRange[0:3])
+        endDate = datetime(*args.dateRange[3:])
+    if args.sc_id is None:
+        args.sc_id = ['A', 'B']
+
+    print('Downloading data from', startDate.date(), 'to', endDate.date())
     delta = endDate - startDate
-    for sc_id in ['A', 'B']:
-        for i in range(delta.days + 1):
-            date = startDate + timedelta(days=i)
-            print(date)
-            try:
-                saveEMFISISSpectra(sc_id, date, '/home/mike/research/rbsp/'
-                'data/emfisis/rbsp{}'.format(sc_id.lower()), 
-                    spectraType = 'spectral-matrix-diagonal')
-            except urllib.error.HTTPError as err:
-                print(str(err))
-                continue
+    for (sc_id, i) in itertools.product(args.sc_id, range(delta.days + 1)):
+        d = startDate + timedelta(days=i)
+        print('Downloading EMFISIS from: RBSP-{} on {}'.format(sc_id.upper(), d.date()))
+        try:
+            saveEMFISISSpectra(sc_id, d, '/home/mike/research/rbsp/'
+            'data/emfisis/rbsp{}'.format(sc_id.lower()), 
+                spectraType = 'spectral-matrix-diagonal')
+        except urllib.error.HTTPError as err:
+            print(str(err))
+            print('RBSP-{}, date={}'.format(sc_id.upper(), d.date()))
+            continue
+
+
+# for sc_id in args.sc_id:
+#     for i in range(delta.days + 1):
+#         print(sc_id, startDate + timedelta(days=i))
+
+    
+    # startDate = datetime(2015, 2, 2)
+    # endDate =  datetime(2015, 2, 2)
+    # delta = endDate - startDate
+    # for sc_id in ['A', 'B']:
+    #     for i in range(delta.days + 1):
+    #         date = startDate + timedelta(days=i)
+    #         print(date)
+    #         try:
+    #             saveEMFISISSpectra(sc_id, date, '/home/mike/research/rbsp/'
+    #             'data/emfisis/rbsp{}'.format(sc_id.lower()), 
+    #                 spectraType = 'spectral-matrix-diagonal')
+    #         except urllib.error.HTTPError as err:
+    #             print(str(err))
+    #             continue
--- rbsp/download_day_mageis.py
+++ rbsp/download_day_mageis.py
@@ -15,7 +15,7 @@ def downloadMagEIS(rb_id, dates, fPath, dataLevel=2):
         dates = [dates]
         
     for date in dates:
-        print('Downloading RBSP-{} MagEIS data from {}'.format(rb_id.upper(), date))
+        print('Downloading RBSP-{} MagEIS data from {}'.format(rb_id.upper(), date.date()))
         if dataLevel == 2:
             url = ('https://rbsp-ect.lanl.gov/data_pub/rbsp{}/mageis/level2/sectors/'
                 '{}/'.format(rb_id.lower(), date.year))
@@ -29,6 +29,8 @@ def downloadMagEIS(rb_id, dates, fPath, dataLevel=2):
         response = urllib.request.urlopen(url)
         data = response.read().decode()  # Get HTML data to find filename
         fName = findFilename(rb_id, joinDate, data)      
+        if fName is None:  
+            continue
             
         # Download data
         urllib.request.urlretrieve(url + fName, os.path.join(fPath, fName))
@@ -40,18 +42,55 @@ def findFilename(rb_id, joinDate, data):
     filename to download
     """
     regex = re.compile(r'rbsp{}_rel\d\d_ect-mageis-L\d_{}_v'
-        '\d.\d.\d.cdf'.format(rb_id, joinDate))
+        '\d.\d.\d.cdf'.format(rb_id.lower(), joinDate))
     mo = regex.search(data)
-    return mo.group()
+    try:
+        return mo.group()
+    except AttributeError as err:
+        print(err, 'Probably no data was found on this date.')
+        return None
     
 if __name__ == '__main__':
-    START_DATE = datetime(2017, 11, 18)
-    END_DATE = datetime(2017, 11, 19)
-    dates = [START_DATE + timedelta(days=d) for d in 
-        range((END_DATE - START_DATE).days)]
+    import argparse
+    import itertools
+    parser = argparse.ArgumentParser(description=('This script downloads the '
+            'MagEIS data from a website defined in the code.'))
+    parser.add_argument('-sc', '--sc_id', help=('Define a spaceraft id. '
+                                            'Use either "a" or "b". If '
+                                            'none, will download data '
+                                            'from both RBSP spacecraft.'),
+                                            type=str, nargs=1)
+    parser.add_argument('date', default=[], nargs='*', type=int,
+                        help=('This is an optional date positional argument.'
+                                ' The format is YYYY MM DD.'))
+    parser.add_argument('-dr', '--dateRange', default=[], nargs='*', type=int,
+                            help=('This optional argument passes in a start'
+                                    ' and end dates to define a date range for '
+                                    'which the script will download EMFISIS data.'
+                                    ' The format is YYYY MM DD and the input is '
+                                    'size 6'))
+    args = parser.parse_args()
+    if args.date != []:
+        if len(args.date) != 3:
+            raise ValueError('Date not formated correctly! Check manual.')
+        startDate = datetime(*args.date)
+        endDate = datetime(*args.date)
+    if args.dateRange != []:
+        if len(args.dateRange) != 6:
+            raise ValueError('Date range not formated correctly! Check manual.')
+        startDate = datetime(*args.dateRange[0:3])
+        endDate = datetime(*args.dateRange[3:])
+    if args.sc_id is None:
+        args.sc_id = ['A', 'B']
+
+
+    # START_DATE = datetime(2017, 11, 18)
+    # END_DATE = datetime(2017, 11, 19)
+    dates = [startDate + timedelta(days=d) for d in 
+        range((endDate - startDate).days)]
     #for (rb_id, day) in itertools.product(['a', 'b'], dates):
-    for rb_id in ['a', 'b']:
-        fPath = '/home/mike/research/rbsp/data/mageis/rbsp{}'.format(rb_id)
+    for rb_id in args.sc_id:
+        fPath = '/home/mike/research/rbsp/data/mageis/rbsp{}'.format(rb_id.lower())
         downloadMagEIS(rb_id, dates, fPath, dataLevel=3)
     
 
