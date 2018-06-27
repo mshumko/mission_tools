ac43c8670db59a079252d8733d467b8bd01302e1
mshumko <msshumko@gmail.com>
2018-06-26 21:59:44 -0600

    Updated TLE tables.

--- orbit/make_ephem.py
+++ orbit/make_ephem.py
@@ -339,7 +339,7 @@ if __name__ == '__main__':
             tleDir='/home/mike/research/firebird/tle'
         tableObj = Make_TLE_table(sc_id, tleDir=tleDir)
         tableObj.createTable()
-        tBounds = [datetime(2018, 6, 5), datetime(2018, 7, 5)]
+        tBounds = [datetime(2018, 6, 24), datetime(2018, 7, 25)]
         dT = 60
         ephemObj = Make_ephem(sc_id, tBounds, dT)
         ephemObj.loadTleTable()
