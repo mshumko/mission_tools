d603df3c718d14dfad102ead51fff5e5a5b625ac
Mykhaylo Mike Shumko <msshumko@gmail.com>
2018-10-02 14:18:32 -0600

    Synced the awesome context data plotter.

---
+++ firebird/plot_context.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+"""
+This script plots the context data from the FIREBIRD-II mission.
+It accepts sc_id (3 or 4) and campaign number arguments, and 
+looks for the most recent data in the firebird/Datafiles
+firectory.
+"""
+import spacepy.datamodel
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import dates
+import matplotlib.colors as colors
+import cartopy.crs as ccrs
+from matplotlib.ticker import FuncFormatter, MaxNLocator
+import dateutil.parser
+from datetime import datetime, timedelta
+import glob
+import os
+import argparse
+
+### ARGPARSE COMMANDS
+parser = argparse.ArgumentParser(description=('This script plots the '
+        'FIREBIRD-II Context level 2 data. No time correction is applied.'))
+parser.add_argument('sc_id',  type=int,
+    help=('This is the FIREBIRD-II unit arument (3 or 4)'))
+parser.add_argument('camp', type=int,
+    help=('This is the FIREBIRD-II campaign number'))     
+args = parser.parse_args()
+
+### FIND DATA ###
+dataDir = ('/home/mike/research/firebird/Datafiles/FU_{}/context/'
+            'level2'.format(args.sc_id))
+fNames = glob.glob(os.path.join(dataDir, 
+                    'FU{}_Context_camp{:02}_*.txt'.format(
+                    args.sc_id, args.camp)))
+# Get the latest campaign file if there are multiple files.
+fname = sorted(fNames)[-1]
+
+### READ DATA ###
+d = spacepy.datamodel.readJSONheadedASCII(
+    os.path.join(dataDir, fname))
+d['Time'] = np.array([dateutil.parser.parse(i) for i in d['Time']])
+
+### PLOT CONTEXT ###
+fig, ax = plt.subplots(figsize=(12, 8))
+ax.plot(d['Time'], d['D0'], label='D0')
+ax.plot(d['Time'], d['D1'], label='D1')
+ax.legend(loc=1)
+ax.set(yscale='log', ylabel='Counts/6 s',
+    title='FU{} Context'.format(args.sc_id)
+    )
+
+### FORMAT X-AXIS to show more information ###
+d['McIlwainL'] = np.round(d['McIlwainL'], decimals=1)
+L = np.copy(d['McIlwainL']).astype(object)
+L[L > 100] = ''
+# This code is a nifty way to format the x-ticks to my liking.
+Labels = ['{}\n{}\n{}\n{}\n{}\n{}'.format(
+            t.replace(microsecond=0).date(), t.replace(microsecond=0).time(),
+            L, round(MLT,1), round(lat,1), round(lon,1)) for 
+            (t, L, MLT, lat, lon) in zip(
+            d['Time'][::10], L[::10], d['MLT'][::10], 
+            d['Lat'][::10], d['Lon'][::10])]  
+numTimes = dates.date2num(d['Time'][::10])
+
+def format_fn(tick_val, tick_pos):
+    """
+    The tick magic happens here. pyplot gives it a tick time, and this function 
+    returns the closest label to that time. Read docs for FuncFormatter().
+    """
+    idx = np.argmin(np.abs(numTimes-tick_val))
+    return Labels[idx]
+
+ax.xaxis.set_major_formatter(FuncFormatter(format_fn))
+ax.set_xlabel('date\ntime\nL\nMLT\nlan\nlon')
+ax.xaxis.set_label_coords(-0.1,-0.03)
+plt.tight_layout()
+plt.subplots_adjust(bottom=0.15)
+
+# Initialize interactive sesson
+def mouseTime(event):
+    """ 
+    When a user presses 't', this function will print the spacecraft info.
+    """
+    if event.key == 't':
+        time = dates.num2date(event.xdata).replace(tzinfo=None).isoformat()
+        Lt = L[np.argmin(np.abs(event.xdata - dates.date2num(d['Time'])))]
+        print(time, 'L =',Lt)
+    elif event.key == 'm':
+        tRange = [t.replace(tzinfo=None) for t in dates.num2date(ax.get_xlim())]
+        plot_map(tRange)
+        #time = dates.num2date(event.xdata).replace(tzinfo=None)
+        # plot_map(time)
+    return
+
+def plot_map(tRange, channel='D0'):
+    """
+    This function plots the map of the orbit +/- 10 minutes of the click time.
+    """
+    # tRange = [time - timedelta(minutes=windowWidth/2), 
+    #             time + timedelta(minutes=windowWidth/2)]
+    idx = np.where((d['Time'] > tRange[0]) & 
+                   (d['Time'] < tRange[1]))[0]
+    fig = plt.figure(figsize=(12, 6))
+    # Nothern hemisphere
+    ax = plt.subplot(121, projection=ccrs.Orthographic(central_latitude=90))
+    ax.stock_img()
+    ax.scatter(d['Lon'][idx], d['Lat'][idx], c=d[channel][idx],
+                transform=ccrs.Geodetic(), norm=colors.LogNorm())
+    # Southern hemisphere
+    bx = plt.subplot(122, projection=ccrs.Orthographic(central_latitude=-90))
+    bx.stock_img()
+    sc = bx.scatter(d['Lon'][idx], d['Lat'][idx], c=d[channel][idx],
+                transform=ccrs.Geodetic(), norm=colors.LogNorm())
+    # Mark starting point with a black star
+    if d['Lat'][idx[0]] > 0:
+        ax.text(d['Lon'][idx[0]], d['Lat'][idx[0]], 'start',
+         horizontalalignment='right',
+         transform=ccrs.Geodetic())
+        # ax.scatter(d['Lon'][idx[0]], d['Lat'][idx[0]], c='w', marker='*',
+        #             transform=ccrs.Geodetic())
+    else:
+        bx.text(d['Lon'][idx[0]]-3, d['Lat'][idx[0]]-3, 'start',
+         horizontalalignment='right',
+         transform=ccrs.Geodetic())
+        # bx.scatter(d['Lon'][idx[0]], d['Lat'][idx[0]], c='w', marker='*',
+        #             transform=ccrs.Geodetic())
+
+    fig.suptitle('FU{} context {}\n{} to {}'.format(
+        args.sc_id, tRange[0].date(), tRange[0].replace(microsecond=0).time(), 
+        tRange[1].replace(microsecond=0).time()), fontsize=16)
+    plt.tight_layout()
+    # Plot colorbar
+    fig.subplots_adjust(right=0.89)
+    cbar_ax = fig.add_axes([0.9, 0.15, 0.05, 0.7])
+    fig.colorbar(sc, orientation='vertical', cax=cbar_ax,
+            label='{} [counts/6s]'.format(channel))
+    plt.show()
+    return
+
+ax.figure.canvas.mpl_connect('key_press_event', mouseTime)
+plt.show()
