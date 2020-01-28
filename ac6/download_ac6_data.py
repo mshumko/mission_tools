3e85ea8f4d34cc7a6e722189c30e10cf7017eed5
Mykhaylo Mike Shumko <msshumko@gmail.com>
2020-01-28 13:57:22 -0700

    Updated the AC6 data downloader using Emil's code as a starting point.

--- ac6/download_ac6_data.py
+++ ac6/download_ac6_data.py
@@ -1,64 +1,155 @@
-# This code downloads AC6 data for the days specified
-import requests
-import urllib
+import os
+import urllib3
 import tarfile
 import io
-from datetime import datetime, timedelta
-# import itertools
+import glob
+import argparse
+from datetime import datetime, date, timedelta
 
-def download_ac6_day(sc_id, date, saveDir=None):
+def download_ac6_data(sc_id, data_date, save_dir, verbose=True):
     """
-    This function downloads AC-6 data from the RBSP science gateway and 
-    saves it to a saveDir. The tgz file is uncompressed.
+    This function downloads AC6 data.
+
+    Parameters
+    ----------
+    sc_id : str
+        Spacecraft id, either A or B. It is case insensitive.
+    data_date : str, datetime, or date object
+        The date you need the path for. The date can a string 
+        formatted as, for example, 20161010 for 10 Oct 2016 
+        date. The date can also be a datetime or date object.
+    save_dir : str
+        The path where to save the AC6 csv files.
+    verbose : bool, optional
+        If true will print a status message if data was or was not
+        downloaded for that day.
+
+    Returns
+    -------
+    None
     """
-    print( date.year)
-    url = ('http://rbspgway.jhuapl.edu/share/ac6/data/'
-        'AC6-{}/{}/AC6-{}_{}_V03.tgz'.format(
-            sc_id.upper(), date.year, sc_id.upper(), date.date().isoformat().replace('-', '')))
-    print(url)
-    #request = requests.get(url) # Get tgz file
-    request = urllib.request.urlopen(url)
-    #print(request)
-    #tar = tarfile.open(io.BytesIO(request.read()), 'r')
-    #print(tar)
-    return request
+    file_path = get_ac6_path(sc_id, date)
+
+    http = urllib3.PoolManager()
+    f = http.request('GET', file_path)
+    tarball = io.BytesIO(f.data)
+
+    try:
+        # If tarball file found, extract to save_dir and exit.
+        tar = tarfile.open('r', fileobj=tarball)
+        tar.extractall(save_dir)
+        # Rename the files from the YYYYDDMM convention to YYYYMMDD
+        rename_files(sc_id, data_date, save_dir) 
+        if verbose: print(f'Data for AC6-{sc_id.upper()} on {data_date} downloaded')
+    except tarfile.ReadError:
+        # If file not found, print error if verbose and exit.
+        if verbose: print(f'Data for AC6-{sc_id.upper()} on {data_date} could not '
+                          'be downloaded')
+    return
+
+def get_ac6_path(sc_id, data_date, 
+                base_url='http://rbspgway.jhuapl.edu/share/ac6/data/'):
+    """
+    This function returns a url path for the AC6 data given 
+    spacecraft id and date. The date can be either a string
+    formatted as 20161010 for 10 Oct 2016 date.
+
+    Parameters
+    ----------
+    sc_id : str
+        Spacecraft id, either A or B. It is case insensitive.
+    data_date : str, datetime, or date object
+        The date you need the path for. The date can a string 
+        formatted as, for example, 20161010 for 10 Oct 2016 
+        date. The date can also be a datetime or date object.
+
+    Returns
+    -------
+    file_path : str 
+        A complete file path (url) that specifies where the AC6 
+        data lives on the RBSP science gateway for spacecraft 
+        sc_id on date data_date.
+    """
+    if isinstance(data_date, str):
+        # if data_date formatted as 20161010 for 10 Oct 2016 date.
+        year = data_date[:4]
+        file_dir = f'AC6-{sc_id.upper()}/{year}/'
+        file_name = f'AC6-{sc_id.upper()}_{data_date}_V03.tgz'
+    elif isinstance(data_date, datetime) or isinstance(data_date, date):
+        # Otherwise if the data_date is a datetime or date object.
+        file_dir = f'AC6-{sc_id.upper()}/{data_date.year}/'
+        file_name = f'AC6-{sc_id.upper()}_{data_date.strftime("%Y%d%m")}_V03.tgz'
+    else:
+        raise ValueError('The data_data argument must be a string,'
+                        ' datetime, or date object.')
+    file_path = os.path.join(base_url, file_dir, file_name)
+    return file_path
+
+def rename_files(sc_id, data_date, save_path):
+    """
+    This function rename the downloaded AC6 files from the 
+    YYYYDDMM convention to YYYYMMDD
+
+    Parameters
+    ----------
+    sc_id : str
+        Spacecraft id, either A or B. It is case insensitive.
+    data_date : str, datetime, or date object
+        The date you need the path for. The date can a string 
+        formatted as, for example, 20161010 for 10 Oct 2016 
+        date. The date can also be a datetime or date object.
+    save_dir : str
+        The path where to save the AC6 csv files.
+
+    Returns
+    -------
+    None
+    """
+    old_file_search_string = (f'AC6-{sc_id.upper()}_'
+                            f'{data_date.strftime("%Y%d%m")}_*.csv')
+    old_files = glob.glob(os.path.join(save_path, old_file_search_string))
+
+    for old_file_path in old_files:
+        split_path = old_file_path.split('_')
+        split_path[-4] = data_date.strftime('%Y%m%d')
+        new_file_path = '_'.join(split_path)
+        os.rename(old_file_path, new_file_path)
+    return
+
 
 if __name__ == '__main__':
-    import argparse
-    import itertools
-    parser = argparse.ArgumentParser(description=('This script downloads the '
-            'AC6 data from a website defined in the code.'))
-    parser.add_argument('-sc', '--sc_id', help=('Define a spaceraft id. '
-                                            'Use either "a" or "b". If '
-                                            'none, will download data '
-                                            'from both AC-6 spacecraft.'),
-                                            type=str, nargs=1)
-    parser.add_argument('date', default=[], nargs='*', type=int,
-                        help=('This is an optional date positional argument.'
-                                ' The format is YYYY MM DD.'))
-    parser.add_argument('-dr', '--dateRange', default=[], nargs='*', type=int,
-                            help=('This optional argument passes in a start'
-                                    ' and end dates to define a date range for '
-                                    'which the script will download EMFISIS data.'
-                                    ' The format is YYYY MM DD and the input is '
-                                    'size 6'))
+    """
+    This script wrapps the download_ac6_data function and uses command line
+    arguments to download AC6 data from multiple days.
+
+    For example, if you want data from both AC6 units from 1 October 2016 to
+    31 October 2016 call:
+
+    python3 download_ac6_data.py A B 2016 10 1 2016 10 31
+    """
+
+    parser = argparse.ArgumentParser(
+                    description=('This script downloads AC6 data from '
+                                'the RBSP science gateway.'))
+    parser.add_argument('sc_id', type=str, nargs='+',
+        help=('Spacecraft to download data from. '
+        'Can be "A", "B", or "A B".'))
+    parser.add_argument('dates', nargs=6, type=int,
+        help=('Start and end dates to download. Must be in the following format '
+        'YYYY MM DD YYY MM DD.')) 
+    parser.add_argument('-sd', '--save_dir', type=str, 
+        default='/home/mike/Downloads/ac6', 
+        help=('Directory where to save the data to.'))
     args = parser.parse_args()
-    if args.date != []:
-        if len(args.date) != 3:
-            raise ValueError('Date not formated correctly! Check manual.')
-        startDate = datetime(*args.date)
-        endDate = datetime(*args.date)
-    if args.dateRange != []:
-        if len(args.dateRange) != 6:
-            raise ValueError('Date range not formated correctly! Check manual.')
-        startDate = datetime(*args.dateRange[0:3])
-        endDate = datetime(*args.dateRange[3:])
-    if args.sc_id is None:
-        args.sc_id = ['A', 'B']
 
-    # Loop over the dates:
-    dates = [startDate + timedelta(days=d) for d in 
-        range(1+(endDate - startDate).days)]
+    # Figure out which days to download the data from.
+    start_date = datetime(*args.dates[:3])
+    end_date = datetime(*args.dates[3:])
+    dates = [start_date + timedelta(days=i) 
+            for i in range((end_date-start_date).days+1)]
 
-    for (sc_id, day) in itertools.product(args.sc_id, dates):
-        r  =download_ac6_day(sc_id, day)
+    for date in dates:
+        for sc_id in args.sc_id:
+            download_ac6_data(sc_id, date, args.save_dir)
+
+
