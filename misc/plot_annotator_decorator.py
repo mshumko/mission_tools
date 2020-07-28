9d01d6836b3a4a28d91987d91cfe1ba89f3707a4
mike <msshumko@gmail.com>
2020-07-28 17:55:27 -0600

    Small changes.

--- misc/plot_annotator_decorator.py
+++ misc/plot_annotator_decorator.py
@@ -18,6 +18,9 @@ def plot_sine_plt():
     x = np.linspace(0, 2*np.pi)
     y = np.sin(x)
     plt.plot(x, y)
+    plt.title('The greatest sine curve in the land')
+    plt.xlabel('x')
+    plt.ylabel('y')
     return
 
 @annotate_plot
@@ -26,6 +29,9 @@ def plot_sine_subplot():
     y = np.sin(x)
     fig, ax = plt.subplots()
     ax.plot(x, y)
+    ax.set_title('The greatest sine curve in the land')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
     return
 
 @annotate_plot
@@ -35,6 +41,9 @@ def plot_sine_subplots():
     fig, ax = plt.subplots(2, 1)
     ax[0].plot(x, y)
     ax[1].plot(x, y)
+    ax[0].set_title('The greatest sine curves in the land')
+    ax[-1].set_xlabel('x')
+    ax[-1].set_ylabel('y')
     return
 
 
